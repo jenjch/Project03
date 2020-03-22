cd420e697ccd5c7d1ae791ff67890a9d6cd41707
Jen C <jenjch8@gmail.com>
2020-03-22 10:24:39 -0500

    small fix to input functions

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -4,7 +4,7 @@ import {Input} from "../Form";
 function Login(props) {
 
   let [email, setEmail] = useState ("")
-  // let [password, setPassword] = useState ("")
+  let [password, setPassword] = useState ("")
 
   const handleEmail = event => {
     setEmail(event.target.value);
@@ -12,12 +12,13 @@ function Login(props) {
   }
 
   const handlePassword = event => {
-    setEmail(event.target.value);
+    setPassword(event.target.value);
     // console.log(event.target.value)
   }
 
   const handleClick = () => {
     console.log(email);
+    console.log(password);
   }
 
     return (
